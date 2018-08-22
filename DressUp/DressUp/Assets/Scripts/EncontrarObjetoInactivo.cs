98d5529b5940e02de67d13e53030d68fb44aab6a
oislas03 <oislas03@gmail.com>
2018-08-21 20:31:34 -0700

    Working master file
    Chameleon starts here

---
+++ DressUp/DressUp/Assets/Scripts/EncontrarObjetoInactivo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EncontrarObjetoInactivo {
+
+    /// <summary>
+    /// Este método permite encontrar un GameObject inactivo en la escena seleccionada 
+    /// </summary>
+    /// <param name="parent">GameObject padre que contiene al GameObject buscado</param>
+    /// <param name="name">Nombre del GameObject a buscar</param>
+    /// <returns></returns>
+    static public GameObject EncontrarObjeto(this GameObject parent, string name)
+    {
+        Transform[] trs = parent.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in trs)
+        {
+            if (t.name == name)
+            {
+                return t.gameObject;
+            }
+        }
+        return null;
+    }
+}
---
+++ DressUp/DressUp/Assets/Scripts/OpcionSeleccionada.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpcionSeleccionada : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CambiarFondo(string nombreFondo)
+    {
+        nombreFondo = "fondo2.jpeg";
+        GameObject obj = GameObject.Find("Fondo");
+
+        Image imagen = obj.GetComponent<Image>();
+        Sprite sprite = Resources.Load("Sprites/" + nombreFondo + ".png") as Sprite;
+        
+        imagen.sprite = sprite;
+    }
+}
